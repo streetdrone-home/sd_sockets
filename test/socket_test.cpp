9bf30be22d46da64fd04a8b1eccb7ec4f44a7ad8
Isaac Vander Sluis <isaac.vandersluis@streetdrone.com>
2022-10-25 14:52:10 +0100

    Use threading to test client-server interactions

--- test/socket_test.cpp
+++ test/socket_test.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <gtest/gtest.h>
+#include <thread>
 
+#include "gtest/gtest.h"
 #include "sd_sockets/sd_sockets.hpp"
 
 TEST(is_open_test, check_closed_on_init)
@@ -25,4 +26,53 @@ TEST(is_open_test, check_closed_on_init)
   EXPECT_FALSE(socket.is_open());
   EXPECT_FALSE(client.is_open());
   EXPECT_FALSE(server.is_open());
+}
+
+TEST(is_open_test, check_open_after_connect)
+{
+  auto socket = sd_sockets::Socket{};
+  auto client = sd_sockets::Client{};
+  auto server = sd_sockets::Server{15243};
+
+  std::thread t1([&]() { server.accept(); });
+  client.connect("127.0.0.1", 15243);
+  t1.join();
+
+  EXPECT_FALSE(socket.is_open());
+  EXPECT_TRUE(client.is_open());
+  EXPECT_TRUE(server.is_open());
+}
+
+TEST(read_write_test, server_to_client)
+{
+  auto input = "Hello, World!";
+  auto client = sd_sockets::Client{};
+  auto server = sd_sockets::Server{15243};
+
+  std::thread t1([&]() { server.accept(); });
+  client.connect("127.0.0.1", 15243);
+  t1.join();
+
+  std::thread t2([&]() { server.write(input); });
+  auto output = client.read();
+  t2.join();
+
+  EXPECT_EQ(input, output) << "Expected " << input << ", got " << output;
+}
+
+TEST(read_write_test, client_to_server)
+{
+  auto input = "Hello, World!";
+  auto client = sd_sockets::Client{};
+  auto server = sd_sockets::Server{15243};
+
+  std::thread t1([&]() { server.accept(); });
+  client.connect("127.0.0.1", 15243);
+  t1.join();
+
+  std::thread t2([&]() { client.write(input); });
+  auto output = server.read();
+  t2.join();
+
+  EXPECT_EQ(input, output) << "Expected " << input << ", got " << output;
 }
